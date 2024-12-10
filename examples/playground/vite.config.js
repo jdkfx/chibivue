3398eb25933b69261852d3749aa4b959db5773c2
jdkfx <htazoe@jdkfx.com>
2024-12-10 23:00:34 +0900

    init

---
+++ examples/playground/vite.config.js
@@ -0,0 +1,12 @@
+import path, { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite';
+
+const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
+export default defineConfig({
+  resolve: {
+    alias: {
+      chibivue: path.resolve(dirname, '../../packages'),
+    },
+  },
+})
