368efc78e166b4c93b68776dbaceca8c48423831
jdkfx <htazoe@jdkfx.com>
2025-01-25 23:55:00 +0900

    update

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,6 +1,6 @@
 import { ReactiveEffect } from "../reactivity";
-import { Component } from "./component";
-import { normalizeVNode, Text, VNode } from "./vnode"
+import { Component, ComponentInternalInstance, createComponentInstance, InternalRenderFunction } from "./component";
+import { createVNode, normalizeVNode, Text, VNode } from "./vnode"
 
 export interface RendererOptions<
   HostNode = RendererNode,
@@ -12,6 +12,7 @@ export interface RendererOptions<
   setText(node: HostNode, text: string): void
   setElementText(node: HostNode, text: string): void
   insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void
+  parentNode(node: HostNode): HostNode | null
 }
 
 export interface RendererNode {
@@ -32,29 +33,24 @@ export function createRenderer(options: RendererOptions) {
     createText: hostCreateText,
     setText: hostSetText,
     insert: hostInsert,
+    parentNode: hostParentNode,
   } = options
 
   const render: RootRenderFunction = (rootComponent, container) => {
-    const componentRender = rootComponent.setup!()
-
-    let n1: VNode | null = null
-
-    const updateComponent = () => {
-      const n2 = componentRender()
-      patch(n1, n2, container)
-      n1 = n2
-    }
-
-    const effect = new ReactiveEffect(updateComponent)
-    effect.run()
+    const vnode = createVNode(rootComponent, {}, [])
+    patch(null, vnode, container)
   }
 
   const patch = (n1: VNode | null, n2: VNode, container: RendererElement) => {
     const { type } = n2
     if (type === Text) {
       processText(n1, n2, container);
-    } else {
+    } else if (typeof type === 'string') {
       processElement(n1, n2, container);
+    } else if (typeof type === 'object') {
+      processComponent(n1, n2, container);
+    } else {
+      // do nothing
     }
   }
 
@@ -68,6 +64,75 @@ export function createRenderer(options: RendererOptions) {
     } else {
       patchElement(n1, n2)
     }
+  }
+
+  const processComponent = (
+    n1: VNode | null,
+    n2: VNode,
+    container: RendererElement,
+  ) => {
+    if (n1 == null) {
+      mountComponent(n2, container)
+    } else {
+      updateComponent(n1, n2)
+    }
+  }
+
+  const mountComponent = (initialVNode: VNode, container: RendererElement) => {
+    const instance: ComponentInternalInstance = (initialVNode.component =
+      createComponentInstance(initialVNode))
+
+    const component = initialVNode.type as Component
+    if (component.setup) {
+      instance.render = component.setup() as InternalRenderFunction
+    }
+
+    setupRenderEffect(instance, initialVNode, container)
+  }
+
+  const setupRenderEffect = (
+    instance: ComponentInternalInstance,
+    initialVNode: VNode,
+    container: RendererElement,
+  ) => {
+    const componentUpdateFn = () => {
+      const { render } = instance
+
+      if (!instance.isMounted) {
+        const subTree = (instance.subTree = normalizeVNode(render()))
+        patch(null, subTree, container)
+        initialVNode.el = subTree.el
+        instance.isMounted = true
+      } else {
+        let { next, vnode } = instance
+        
+        if (next) {
+          next.el = vnode.el
+          next.component = instance
+          instance.vnode = next
+          instance.next = null
+        } else {
+          next = vnode
+        }
+
+        const prevTree = instance.subTree
+        const nextTree = normalizeVNode(render())
+        instance.subTree = nextTree
+
+        patch(prevTree, nextTree, hostParentNode(prevTree.el!)!)
+        next.el = nextTree.el
+      }
+    }
+
+    const effect = (instance.effect = new ReactiveEffect(componentUpdateFn))
+    const update = (instance.update = () => effect.run())
+    update()
+  }
+
+  const updateComponent = (n1: VNode, n2: VNode) => {
+    const instance = (n2.component = n1.component)!
+    instance.next = n2
+    instance.update()
   }
 
   const mountElement = (vnode: VNode, container: RendererElement) => {
