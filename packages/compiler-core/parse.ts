aa0bfc5e1ed31136c5a3ef4ff4d23619e1dab94d
jdkfx <htazoe@jdkfx.com>
2025-02-05 23:45:58 +0900

    update

--- packages/compiler-core/parse.ts
+++ packages/compiler-core/parse.ts
@@ -1,16 +1,95 @@
+import { ElementNode, NodeTypes, TemplateChildNode } from "./ast";
+
+export interface ParserContext {
+	readonly originalSource: string;
+	source: string;
+	offset: number;
+	line: number;
+	column: number;
+}
+
+function createParserContext(content: string): ParserContext {
+	return {
+		originalSource: content,
+		source: content,
+		column: 1,
+		line: 1,
+		offset: 0,
+	};
+}
+
 export const baseParse = (
 	content: string,
-): { tag: string; props: Record<string, string>; textContent: string } => {
-	const matched = content.match(/<(\w+)\s+([^>]*)>([^<]*)<\/\1>/);
-	if (!matched) return { tag: "", props: {}, textContent: "" };
-
-	const [_, tag, attr, textContent] = matched;
-
-	const props: Record<string, string> = {};
-	attr.replace(/(\w+)=["']([^"']*)["']/g, (_, key: string, value: string) => {
-		props[key] = value;
-		return "";
-	});
-
-	return { tag, props, textContent };
+): { children: TemplateChildNode[] } => {
+	const context = createParserContext(content);
+	const children = parseChildren(context, []);
+	return { children: children };
 };
+
+function parseChildren(
+	context: ParserContext,
+	ancestors: ElementNode[],
+): TemplateChildNode[] {
+	const nodes: TemplateChildNode[] = [];
+
+	while (!isEnd(context, ancestors)) {
+		const s = context.source;
+		let node: TemplateChildNode | undefined;
+
+		if (s[0] === "<") {
+			if (/[a-z]/i.test(s[1])) {
+				node = parseElement(context, ancestors);
+			}
+		}
+
+		if (!node) {
+			node = parseText(context);
+		}
+
+		pushNode(nodes, node);
+	}
+
+	return nodes;
+}
+
+function isEnd(context: ParserContext, ancestors: ElementNode[]): boolean {
+	const s = context.source;
+
+	if (startsWith(s, "</")) {
+		for (let i = ancestors.length - 1; i >= 0; --i) {
+			if (startsWithEndTagOpen(s, ancestors[i].tag)) {
+				return true;
+			}
+		}
+	}
+
+	return !s;
+}
+
+function startsWith(source: string, searchString: string): boolean {
+	return source.startsWith(searchString);
+}
+
+function pushNode(nodes: TemplateChildNode[], node: TemplateChildNode): void {
+	if (node.type === NodeTypes.TEXT) {
+		const prev = last(nodes);
+		if (prev && prev.type === NodeTypes.TEXT) {
+			prev.content += node.content;
+			return;
+		}
+	}
+
+	nodes.push(node);
+}
+
+function last<T>(xs: T[]): T | undefined {
+	return xs[xs.length - 1];
+}
+
+function startsWithEndTagOpen(source: string, tag: string): boolean {
+	return (
+		startsWith(source, "</") &&
+		source.slice(2, 2 | tag.length).toLowerCase() === tag.toLowerCase() &&
+		/[\t\r\n\f />]/.test(source[2 + tag.length] || ">")
+	);
+}
