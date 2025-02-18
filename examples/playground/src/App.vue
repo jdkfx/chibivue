30d6506cb3b94adcf4ae836084453c8d08cf9a1c
jdkfx <htazoe@jdkfx.com>
2025-02-18 22:17:31 +0900

    update

---
+++ examples/playground/src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import { reactive } from "chibivue";
+
+export default {
+	setup() {
+		const state = reactive({ message: "Hello, chibivue!", input: "" });
+
+		const changeMessage = () => {
+			state.message += "!";
+		};
+
+		const handleInput = (e) => {
+			state.input = e.target?.value ?? "";
+		};
+
+		return { state, changeMessage, handleInput };
+	},
+};
+</script>
+
+<template>
+    <div class="container" style="text-align: center">
+		<h2>{{ state.message }}</h2>
+        <img
+            width="150px"
+            src="https://avatars.githubusercontent.com/u/40142697?v=4"
+            alt="My profile image"
+        />
+		<p>My name is <b>Haruki Tazoe</b></p>
+
+		<button @click="changeMessage"> click me! </button>
+
+		<br />
+
+		<label>
+			Input Data
+			<input @input="handleInput" />
+		</label>
+
+
+        <p>input value: {{ state.input }}</p>		
+	</div>
+</template>
+
+<style>
+.container{
+    height: 100vh;
+    padding: 16px;
+    background-color: #c0ffee;
+    color: #2c3e50;
+}
+</style>
--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,49 +1,6 @@
 import { createApp, reactive } from "chibivue";
+import App from "./App.vue";
 
-const app = createApp({
-	setup() {
-		const state = reactive({ message: "Hello, Chibivue!", input: "" });
-
-		const changeMessage = () => {
-			state.message += "!";
-		};
-
-		const handleInput = (e: InputEvent) => {
-			state.input = (e.target as HTMLInputElement)?.value ?? "";
-		};
-
-		return { state, changeMessage, handleInput };
-	},
-
-	template: `
-		<div class="container" style="text-align: center">
-			<h2 id="hello">{{ state.message }}</h2>
-			<img
-				width="150px"
-				src="https://avatars.githubusercontent.com/u/40142697?v=4"
-				alt="My profile image"
-			/>
-			<p>My name is <b>Haruki Tazoe</b></p>
-
-			<button @click="changeMessage"> click me! </button>
-
-			<br />
-
-			<label>
-				Input Data
-				<input @input="handleInput" />
-			</label>
-
-			<style>
-				.container{
-					height: 100vh;
-					padding: 16px;
-					background-color: #becdbe;
-					color: #2c3e50;
-				}
-			</style>
-		</div>
-	`,
-});
+const app = createApp(App);
 
 app.mount("#app");
