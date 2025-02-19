2e490cb56e89a5e9f7db3c8d92bd8271aab5f22a
jdkfx <htazoe@jdkfx.com>
2025-02-19 20:49:46 +0900

    update

--- packages/compiler-core/codegen.ts
+++ packages/compiler-core/codegen.ts
@@ -8,55 +8,75 @@ import {
 	TemplateChildNode,
 	TextNode,
 } from "./ast";
+import { CompilerOptions } from "./options";
 
-export const generate = ({
-	children,
-}: {
-	children: TemplateChildNode[];
-}): string => {
-	return `return function render(_ctx) {
-	with (_ctx) {
-		const { h } = Chibivue;
-		return ${genNode(children[0])};
-	}
-}`;
+export const generate = (
+	{
+		children,
+	}: {
+		children: TemplateChildNode[];
+	},
+	option: Required<CompilerOptions>,
+): string => {
+	return `${option.isBrowser ? "return " : ""}function render(_ctx) {
+		${option.isBrowser ? "with (_ctx) {" : ""}
+		  const { h } = ChibiVue;
+		  return ${genNode(children[0], option)};
+		${option.isBrowser ? "}" : ""}
+	}`;
 };
 
-const genNode = (node: TemplateChildNode): string => {
+const genNode = (
+	node: TemplateChildNode,
+	option: Required<CompilerOptions>,
+): string => {
 	switch (node.type) {
 		case NodeTypes.ELEMENT:
-			return genElement(node);
+			return genElement(node, option);
 		case NodeTypes.TEXT:
 			return genText(node);
 		case NodeTypes.INTERPOLATION:
-			return getInterpolation(node);
+			return getInterpolation(node, option);
 		default:
 			return "";
 	}
 };
 
-const genElement = (el: ElementNode): string => {
+const genElement = (
+	el: ElementNode,
+	option: Required<CompilerOptions>,
+): string => {
 	return `h("${el.tag}", {${el.props
-		.map((prop) => genProp(prop))
-		.join(", ")}}, [${el.children.map((it) => genNode(it)).join(", ")}])`;
+		.map((prop) => genProp(prop, option))
+		.join(
+			", ",
+		)}}, [${el.children.map((it) => genNode(it, option)).join(", ")}])`;
 };
 
 const genText = (text: TextNode): string => {
 	return `\`${text.content}\``;
 };
 
-const getInterpolation = (node: InterpolationNode): string => {
-	return `${node.content}`;
+const getInterpolation = (
+	node: InterpolationNode,
+	option: Required<CompilerOptions>,
+): string => {
+	return `${option.isBrowser ? "" : "_ctx."}${node.content}`;
 };
 
-const genProp = (prop: AttributeNode | DirectiveNode): string => {
+const genProp = (
+	prop: AttributeNode | DirectiveNode,
+	option: Required<CompilerOptions>,
+): string => {
 	switch (prop.type) {
 		case NodeTypes.ATTRIBUTE:
 			return `${prop.name}: "${prop.value?.content}"`;
 		case NodeTypes.DIRECTIVE: {
 			switch (prop.name) {
 				case "on":
-					return `${toHandlerKey(prop.arg)}: ${prop.exp}`;
+					return `${toHandlerKey(prop.arg)}: ${
+						option.isBrowser ? "" : "_ctx."
+					}${prop.exp}`;
 				default:
 					throw new Error(`unexpected directive name. got "${prop.name}"`);
 			}
--- packages/compiler-dom/index.ts
+++ packages/compiler-dom/index.ts
@@ -1,7 +1,9 @@
-import { baseCompile, baseParse } from "../compiler-core";
+import { baseCompile, baseParse, CompilerOptions } from "../compiler-core";
 
-export function compile(template: string) {
-	return baseCompile(template);
+export function compile(template: string, options?: CompilerOptions) {
+	const defaultOption: Required<CompilerOptions> = { isBrowser: true };
+	if (options) Object.assign(defaultOption, options);
+	return baseCompile(template, defaultOption);
 }
 
 export function parse(template: string) {
