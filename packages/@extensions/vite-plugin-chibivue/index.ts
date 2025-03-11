c8dd4806e498aaff3ff0704fb9a18d2d40c971c1
jdkfx <htazoe@jdkfx.com>
2025-03-11 20:53:59 +0900

    update

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,4 +1,5 @@
 import type { Plugin } from "vite";
+import * as fs from "fs";
 import { createFilter } from "vite";
 import { compile } from "../../compiler-dom";
 import { parse, rewriteDefault } from "../../compiler-sfc";
@@ -9,11 +10,27 @@ export default function vitePluginChibivue(): Plugin {
 	return {
 		name: "vite:chibivue",
 
+		resolveId(id) {
+			if (id.match(/\.vue\.css$/)) return id
+		},
+
+		load(id) {
+			if (id.match(/\.vue\.css$/)) {
+				const filename = id.replace(/\.css$/, "");
+				const content = fs.readFileSync(filename, "utf-8");
+				const { descriptor} = parse(content, { filename });
+
+				const styles = descriptor.styles.map(it => it.content).join("\n");
+				return { code: styles };
+			}
+		},
+
 		transform(code, id) {
 			if (!filter(id)) return;
 
 			const outputs = [];
-			outputs.push("import * as ChibiVue from 'chibivue'");
+			outputs.push("import * as ChibiVue from 'chibivue';\n");
+			outputs.push(`import '${id}.css';\n`);
 
 			const { descriptor } = parse(code, { filename: id });
 
